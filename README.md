fbe1e04bfba3aea3c37ce070772cfc5b2c705916
Tom Storms <tom@islandmeetscity.com>
2022-03-18 12:01:35 +1000

    Add Slack integration libraries

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ a 'HTTP Request' action from your Stream Deck.
 
 Currently supports:
 - Changing colours of a USB connected [Blinkstick](https://www.blinkstick.com/).
-
+- Updating Slack messages based on button presses
 
 ## Getting Start
 
@@ -61,3 +61,19 @@ URL: `http://localhost:32100/blinkstick/busy/blink-on`
 Icon: Use `./plugins/blinkstick/images/blinkstick-busy-blink.png`
 
 Method: GET
+
+
+## Slack Integration
+
+This functionality helps change your Slack status when you change your Blinkstick.
+
+It does require an API from Slack. To set this up:
+
+- Create a new app from scratch: https://api.slack.com/apps
+- Give it a name like 'StreamDeck Slack - <slack-workspace-name>' and choose the Workspace
+- Go to 'oAuth & Permissions'
+- Scroll down to 'User Token Scopes' and add the 'users.profile:write' 'users:write' scope - basically tells Slack you want this app to change your Slack status
+- Scroll back up and 'Install to Workspace' and follow the prompts to have the app install
+- This will provide a 'User OAuth Token' which you can then add to your `.env` file
+
+
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const port = 32100;
@@ -5,32 +7,72 @@ const port = 32100;
 const blinkstickLib = require('./lib/blinkstick');
 global.blinkstickDevice = blinkstickLib.findFirst();
 
+const slackLib = require('./lib/slack');
+
 const blinkstick = require('./plugins/blinkstick/blinkstick');
 
 app.get('/blinkstick/on', (req, res) => {
-  res.send('Blinkstick On');
   blinkstick.turnOffBusyBlink();
   blinkstick.turnOn();
+  // slackLib.setSlackStatusAvailable();
+  res.send('Blinkstick On');
 });
 
 app.get('/blinkstick/off', (req, res) => {
-  res.send('Blinkstick off');
   blinkstick.turnOffBusyBlink();
   blinkstick.turnOff();
+  res.send('Blinkstick off');
 });
 
 app.get('/blinkstick/busy', (req, res) => {
-  res.send('Blinkstick Busy On');
   blinkstick.turnOffBusyBlink();
   blinkstick.turnBusy();
+  res.send('Blinkstick Busy On');
 });
 
-app.get('/blinkstick/busy/blink-toggle', (req, res) => {
-  res.send('Blinkstick Busy Blinking Toggling');
-  blinkstick.turnOffBusyBlink();
-  blinkstick.toggleBusyBlink();
-});
+// app.get('/blinkstick/lunch', (req, res) => {
+//   // blinkstick.turnOffBusyBlink();
+//   // blinkstick.turnBusy();
+//   // res.send('Blinkstick Busy On');
+// });
+
+// app.get('/blinkstick/busy/blink-toggle', (req, res) => {
+//   blinkstick.turnOffBusyBlink();
+//   blinkstick.toggleBusyBlink();
+//   res.send('Blinkstick Busy Blinking Toggling');
+// });
+
+
+// ---------------------------------------------
+
+// app.get('/slack/available', (req, res) => {
+//   slackLib.setSlackStatusAvailable();  
+//   res.send('Slack - set available');
+// });
+
+// app.get('/slack/away', (req, res) => {
+//   slackLib.setSlackStatusAway();  
+//   res.send('Slack - set away');
+// });
+
+// app.get('/slack/lunch', (req, res) => {
+//   slackLib.setSlackStatusLunch();  
+//   res.send('Slack - set at launch');
+// });
+
+// app.get('/slack/away', (req, res) => {
+//   slackLib.setSlackPresenceAway();  
+//   res.send('Slack - set away');
+// });
+
+// app.get('/slack/online', (req, res) => {
+//   slackLib.setSlackPresenceAuto();  
+//   res.send('Slack - set auto');
+// });
+
+
+// ---------------------------------------------
 
 app.listen(port, () => {
-  console.log(`Blinkstick server running... Port ${port}`)
+  console.log(`Streamdeck HTTP Request server running... Port ${port}`)
 });
